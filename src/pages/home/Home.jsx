90a7328383bde80c20c4339ddb9d4ad33af71404
binayakbhandari <binayakbhandari60@gmail.com>
2024-09-07 20:22:33 +0545

    create footer component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,42 +1,47 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
+import Footer from '../../components/footer/Footer'
 
 const Home = () => {
   return (
-    <div>
-      {/* Navbar */}
+    <>
+      <div>
+        {/* Navbar */}
 
-      <Navbar />
-      {/* Card Container */}
-      <div className="card-container">
-        {/* Card 1 */}
-        <div className="card">
-          <img
-            src="https://via.placeholder.com/150"
-            alt="Placeholder"
-          />
-          <h2>Card Title 1</h2>
-        </div>
+        <Navbar />
+        {/* Card Container */}
+        <div className="card-container">
+          {/* Card 1 */}
+          <div className="card">
+            <img
+              src="https://via.placeholder.com/150"
+              alt="Placeholder"
+            />
+            <h2>Card Title 1</h2>
+          </div>
 
-        {/* Card 2 */}
-        <div className="card">
-          <img
-            src="https://via.placeholder.com/150"
-            alt="Placeholder"
-          />
-          <h2>Card Title 2</h2>
-        </div>
+          {/* Card 2 */}
+          <div className="card">
+            <img
+              src="https://via.placeholder.com/150"
+              alt="Placeholder"
+            />
+            <h2>Card Title 2</h2>
+          </div>
 
-        {/* Card 3 */}
-        <div className="card">
-          <img
-            src="https://via.placeholder.com/150"
-            alt="Placeholder"
-          />
-          <h2>Card Title 3</h2>
+          {/* Card 3 */}
+          <div className="card">
+            <img
+              src="https://via.placeholder.com/150"
+              alt="Placeholder"
+            />
+            <h2>Card Title 3</h2>
+          </div>
         </div>
       </div>
-    </div>
+
+      <Footer />
+    </>
     
   )
 }
